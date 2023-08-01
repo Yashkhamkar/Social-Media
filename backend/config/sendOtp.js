bf074d865cafa6031787ca1fdcd4181492ddfdce
Yash Khamkar <yashkhamkar1100@gmail.com>
2023-08-01 22:32:57 +0530

    Update sendOtp.js

--- backend/config/sendOtp.js
+++ backend/config/sendOtp.js
@@ -1,30 +1,39 @@
 const nodemailer = require("nodemailer");
-const SendOtp = async (email, otp, sub) => {
+
+const SendOtp = (email, otp, sub) => {
   console.log(otp);
-  try {
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
-      auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASS,
-      },
-      secure: true,
-    });
-    const mailData = {
-      to: email,
-      subject: `You have recived Otp from YKONNECT`,
-      text: `${otp} is your YKONNECT OTP. Please do not share the OTP with others.
+
+  return new Promise((resolve, reject) => {
+    try {
+      const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+          user: process.env.NODEMAILER_EMAIL,
+          pass: process.env.NODEMAILER_PASS,
+        },
+        secure: true,
+      });
+
+      const mailData = {
+        to: email,
+        subject: `You have received OTP from YKONNECT`,
+        text: `${otp} is your YKONNECT OTP. Please do not share the OTP with others.
         Regards,
         Team YKONNECT`,
-    };
-    transporter.sendMail(mailData, function (err, info) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
+      };
+
+      transporter.sendMail(mailData, function (err, info) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve("email sent");
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
+
 module.exports = { SendOtp };
